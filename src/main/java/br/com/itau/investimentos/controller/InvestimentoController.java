7ca95538a37e3c3bc36fdf89b87b9f40e49e6717
Caio Pereira <caio.psw@gmail.com>
2020-06-10 20:22:00 -0300

    finish

--- src/main/java/br/com/itau/investimentos/controller/InvestimentoController.java
+++ src/main/java/br/com/itau/investimentos/controller/InvestimentoController.java
@@ -1,12 +1,13 @@
 package br.com.itau.investimentos.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.itau.investimentos.dto.InvestimentoDTO;
@@ -20,20 +21,11 @@ public class InvestimentoController {
 	
 	@GetMapping("/investimento")
 	public List<InvestimentoDTO> getInvestimentos() {
-		
-		List<InvestimentoDTO> lista = new ArrayList<>();
-		InvestimentoDTO teste = new InvestimentoDTO();
-		teste.setMontante(99.99);
-		teste.setQuantidadeMeses(12);
-		teste.setValor(80.27);
-		
-		lista.add(teste);
-		
-		return lista;
-		
+		return investimentoService.consultaInvestimentos();
 	}
 	
 	@PostMapping("/investimento")
+	@ResponseStatus(HttpStatus.CREATED)
 	public InvestimentoDTO createInvestimento(@RequestBody InvestimentoDTO novoInvestimento) {
 		return investimentoService.criaInvestimento(novoInvestimento);
 	}
--- src/main/java/br/com/itau/investimentos/service/InvestimentoService.java
+++ src/main/java/br/com/itau/investimentos/service/InvestimentoService.java
@@ -1,5 +1,8 @@
 package br.com.itau.investimentos.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +15,24 @@ public class InvestimentoService {
 
 	@Autowired
 	private InvestimentoRepository investimentoRepository;
-	
 	private final double taxaJuros = 0.007;
+	
+	public List<InvestimentoDTO> consultaInvestimentos() {
+		
+		final Iterable<InvestimentoEntity> investimentosBD = investimentoRepository.findAll();
+		final List<InvestimentoDTO> investimentos = new ArrayList<>();
+		
+		for (InvestimentoEntity entity: investimentosBD) {
+			final InvestimentoDTO dto = new InvestimentoDTO(
+					entity.getValor(),
+					entity.getQuantidadeMeses(),
+					entity.getMontante());
+			investimentos.add(dto);
+		}
+		
+		return investimentos;
+		
+	}
 	
 	public InvestimentoDTO criaInvestimento(InvestimentoDTO novoInvestimento) {
 		
